fb186ebb1b6f9c5085e6d451d2d224e3534e0265
임지훈 <zhoon95@gmail.com>
2023-07-13 16:02:17 +0900

    Menu_Item_Hover_Effects

---
+++ Menu_Item_Hover_Effects(23_07_13)/README.md
@@ -0,0 +1,6 @@
+# Menu_Item_Hover_Effects
+
+### CodeSandBox
+
+JavaScript : [https://codesandbox.io/s/menu-item-hover-effects-3qf3rt?file=/index.html](https://codesandbox.io/s/menu-item-hover-effects-3qf3rt?file=/index.html) \
+React : []()
--- Particles_Effect/src/App.jsx
+++ Particles_Effect/src/App.jsx
@@ -19,76 +19,139 @@ const App = () => {
       init={particlesInit}
       loaded={particlesLoaded}
       options={{
-        background: {
+        fullScreen: {
+          enable: true,
+          zIndex: 0,
+        },
+        particles: {
+          number: {
+            value: 200,
+            limit: 300,
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+          },
           color: {
-            value: "#0d47a1",
+            value: "#ffffff",
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#000000",
+            },
+            polygon: {
+              nb_sides: 5,
+            },
+            image: {
+              src: "images/github.svg",
+              width: 100,
+              height: 100,
+            },
+          },
+          opacity: {
+            value: 0.5,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0.5,
+              sync: false,
+            },
+          },
+          size: {
+            value: 30,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 10,
+              size_min: 10,
+              sync: false,
+            },
+          },
+          line_linked: {
+            enable: true,
+            distance: 100,
+            color: "#ffffff",
+            opacity: 1,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            speed: 3,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200,
+            },
           },
         },
-        fpsLimit: 120,
         interactivity: {
+          detect_on: "canvas",
           events: {
+            onHover: {
+              enable: true,
+              mode: "bubble",
+              parallax: {
+                enable: false,
+                force: 60,
+                smooth: 10,
+              },
+            },
             onClick: {
               enable: true,
               mode: "push",
             },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
             resize: true,
           },
           modes: {
-            push: {
-              quantity: 4,
+            grab: {
+              distance: 400,
+              lineLinked: {
+                opacity: 1,
+              },
+            },
+            bubble: {
+              distance: 400,
+              size: 100,
+              duration: 2,
+              opacity: 1,
+              speed: 2,
             },
             repulse: {
               distance: 200,
-              duration: 0.4,
+            },
+            push: {
+              particles_nb: 4,
+            },
+            remove: {
+              particles_nb: 2,
             },
           },
         },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            directions: "none",
-            enable: true,
-            outModes: {
-              default: "bounce",
+        backgroundMask: {
+          enable: true,
+          cover: {
+            color: {
+              value: {
+                r: 0,
+                g: 0,
+                b: 0,
+              },
             },
-            random: false,
-            speed: 6,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            value: { min: 1, max: 5 },
           },
         },
-        detectRetina: true,
+        retina_detect: true,
+        fps_limit: 60,
+        background: {
+          image: "url('https://particles.js.org/images/background3.jpg')",
+        },
       }}
     />
   );
