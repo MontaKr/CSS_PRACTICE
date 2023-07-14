8b60fbfad3ccb8b72473828d5eeb29e4bf98bb5d
임지훈 <zhoon95@gmail.com>
2023-07-14 19:01:20 +0900

    React

--- Menu_Item_Hover_Effects(23_07_13)/src/App.jsx
+++ Menu_Item_Hover_Effects(23_07_13)/src/App.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Wrap, Box } from "./styles";
 
 const App = () => {
-  return <div>App</div>;
+  const data = [
+    {
+      text: "Home",
+    },
+    {
+      text: "About",
+    },
+    {
+      text: "Services",
+    },
+    {
+      text: "Portfolio",
+    },
+    {
+      text: "Team",
+    },
+    {
+      text: "Contact",
+    },
+  ];
+
+  return (
+    <Wrap>
+      <ul>
+        {data.map((val) => {
+          return (
+            <Box text={val.text}>
+              <a href="#">{val.text}</a>
+            </Box>
+          );
+        })}
+      </ul>
+    </Wrap>
+  );
 };
 
 export default App;
