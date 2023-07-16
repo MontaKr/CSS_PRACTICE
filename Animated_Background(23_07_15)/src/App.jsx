c88d46f8a284cb93404af61879799fe28b1304b3
임지훈 <zhoon95@gmail.com>
2023-07-16 16:01:11 +0900

    React

--- Animated_Background(23_07_15)/src/App.jsx
+++ Animated_Background(23_07_15)/src/App.jsx
@@ -1,8 +1,54 @@
-import React from "react";
-import { Wrap } from "./styles";
+import React, { useEffect, useState } from "react";
+import { Circle, GlobalStyle } from "./styles";
 
 const App = () => {
-  return <Wrap></Wrap>;
+  const [circles, setCircles] = useState([]);
+
+  useEffect(() => {
+    const createCircle = () => {
+      const sizeW = Math.random() * 12;
+      const duration = Math.random() * 3;
+      const style = {
+        width: `${2 + sizeW}px`,
+        left: `${Math.random() * window.innerWidth}px`,
+        animationDuration: `${2 + duration}s`,
+      };
+
+      setCircles((prev) => [
+        ...prev,
+        {
+          id: Date.now(),
+          style,
+          removeAt: Date.now() + 5000,
+        },
+      ]);
+    };
+
+    const intervalId = setInterval(createCircle, 200);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCircles((prev) =>
+        prev.filter((circle) => Date.now() < circle.removeAt)
+      );
+    }, 500);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <>
+      <GlobalStyle />
+      <div>
+        {circles.map((circle) => (
+          <Circle key={circle.id} style={circle.style} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default App;
--- Animated_Background(23_07_15)/src/styles.jsx
+++ Animated_Background(23_07_15)/src/styles.jsx
@@ -1,3 +1,53 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
-export const Wrap = styled.div``;
+export const GlobalStyle = createGlobalStyle`
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  }
+
+  body {
+    height: 100vh;
+    background: #111;
+    overflow: hidden;
+  }
+`;
+
+export const Circle = styled.div`
+  position: absolute;
+  bottom: 0;
+  aspect-ratio: 1/1;
+  width: 20px;
+  background: #0f0;
+  box-shadow: 0 0 10px #0f0, 0 0 20px #0f0, 0 0 30px #0f0, 0 0 50px #0f0;
+  border-radius: 50%;
+  animation: animate 5s linear forwards;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 25%;
+    width: 50%;
+    opacity: 0.5;
+    height: 100vh;
+    background: linear-gradient(#0f0, transparent);
+  }
+
+  @keyframes animate {
+    0% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+
+    50% {
+      opacity: 1;
+    }
+
+    100% {
+      transform: translateY(-100vh);
+      opacity: 0;
+    }
+  }
+`;
