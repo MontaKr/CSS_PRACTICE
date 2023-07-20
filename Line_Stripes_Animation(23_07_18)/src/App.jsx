df0991569a52cc5f46045202adc2c2dd3073505e
임지훈 <zhoon95@gmail.com>
2023-07-20 16:05:30 +0900

    React

--- Line_Stripes_Animation(23_07_18)/src/App.jsx
+++ Line_Stripes_Animation(23_07_18)/src/App.jsx
@@ -1,8 +1,55 @@
-import React from "react";
-import { Wrap } from "./styles";
+import React, { useEffect, useState } from "react";
+import { Wrap, Line, GlobalStyle } from "./styles";
+import Netflix from "./netflix.png";
 
 const App = () => {
-  return <Wrap></Wrap>;
+  const [lines, setLines] = useState([]);
+
+  useEffect(() => {
+    const createLines = () => {
+      const sizeW = Math.random() * 3;
+      const style = {
+        left: Math.random() * window.innerWidth + "px",
+        width: 1 + sizeW + "px",
+        background: "hsl(" + Math.floor(Math.random() * 60) + "deg,100%, 50%)",
+      };
+
+      setLines((prev) => [
+        ...prev,
+        {
+          id: Date.now(),
+          style,
+          removeAt: Date.now() + 2000,
+        },
+      ]);
+    };
+
+    const intervalId = setInterval(() => {
+      createLines();
+    }, 20);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setLines((lines) => lines.filter((line) => Date.now() < line.removeAt));
+    }, 100);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <>
+      <GlobalStyle />
+      <Wrap>
+        <img src={Netflix} className="logo" />
+        {lines.map((line) => {
+          return <Line key={line.id} style={line.style} />;
+        })}
+      </Wrap>
+    </>
+  );
 };
 
 export default App;
--- Line_Stripes_Animation(23_07_18)/src/styles.jsx
+++ Line_Stripes_Animation(23_07_18)/src/styles.jsx
@@ -1,3 +1,46 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle, keyframes } from "styled-components";
 
-export const Wrap = styled.div``;
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    min-height: 100vh;
+    background: #111;
+    overflow: hidden;
+  }
+`;
+
+export const Wrap = styled.div`
+  .logo {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    max-width: 60vw;
+  }
+`;
+
+export const Line = styled.div`
+  position: absolute;
+  height: 100vh;
+  background: #ea0914;
+  z-index: 1000;
+  mix-blend-mode: color-dodge;
+  width: 50px;
+  animation: animate 2s linear infinite;
+
+  @keyframes animate {
+    0%,
+    100% {
+      opacity: 0;
+    }
+
+    50% {
+      opacity: 1;
+    }
+  }
+`;
