42f29aaae32a4d0ce39da902c86e23094592dd81
임지훈 <zhoon95@gmail.com>
2023-07-21 16:35:21 +0900

    REACT

--- Rainbow_Button(23_07_20)/src/App.jsx
+++ Rainbow_Button(23_07_20)/src/App.jsx
@@ -1,8 +1,41 @@
 import React from "react";
-import { GlobalStyle } from "./styles";
+import { GlobalStyle, Wrap } from "./styles";
 
 const App = () => {
-  return <div>App</div>;
+  const handleMouseMove = (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const x = ((e.clientX - rect.left) / rect.width) * 100;
+    const y = ((e.clientY - rect.top) / rect.height) * 100;
+
+    document.documentElement.style.setProperty("--gradient-pos-x", `${x}%`);
+    document.documentElement.style.setProperty("--gradient-pos-y", `${y}%`);
+  };
+
+  const handleMouseOut = () => {
+    document.documentElement.style.setProperty("--gradient-pos-x", `50%`);
+    document.documentElement.style.setProperty("--gradient-pos-y", `50%`);
+  };
+
+  return (
+    <>
+      <GlobalStyle />
+      <Wrap>
+        <button
+          onMouseMove={handleMouseMove}
+          onMouseOut={handleMouseOut}
+          className="button"
+        >
+          <div className="neon">
+            <div className="gradient"></div>
+          </div>
+          <div className="border">
+            <div className="gradient"></div>
+          </div>
+          <div className="content">Let's Start</div>
+        </button>
+      </Wrap>
+    </>
+  );
 };
 
 export default App;
