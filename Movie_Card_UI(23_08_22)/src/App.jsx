db3b0de452e97391624db8bcc06d811a035cccbf
임지훈 <zhoon95@gmail.com>
2023-08-23 15:28:54 +0900

    REACT

--- Movie_Card_UI(23_08_22)/src/App.jsx
+++ Movie_Card_UI(23_08_22)/src/App.jsx
@@ -1,11 +1,68 @@
 import React from "react";
 import { GlobalStyle, Wrap } from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import poster from "./poster.jpg";
+import logo from "./avtarlogo.png";
+import cast1 from "./cast1.jpg";
+import cast2 from "./cast2.jpg";
+import cast3 from "./cast3.jpg";
+import cast4 from "./cast4.jpg";
+import cast5 from "./cast5.jpg";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <Wrap></Wrap>
+      <Wrap>
+        <div className="card">
+          <div className="poster">
+            <img src={poster} />
+          </div>
+          <div className="details">
+            <img src={logo} className="logo" />
+            <h3>Directed by James Cameron</h3>
+            <div className="rating">
+              <FontAwesomeIcon icon={faStar} className="fa-star" />
+              <FontAwesomeIcon icon={faStar} className="fa-star" />
+              <FontAwesomeIcon icon={faStar} className="fa-star" />
+              <FontAwesomeIcon icon={faStar} className="fa-star" />
+              <i class="fa-regular fa-star"></i>
+              <span>4/5</span>
+            </div>
+            <div className="tags">
+              <span>Sci-fi</span>
+              <span>Action</span>
+            </div>
+            <div className="info">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequuntur itaque laborum cumque natus
+              </p>
+              <div className="cast">
+                <h4>Cast</h4>
+                <ul>
+                  <li>
+                    <img src={cast1} />
+                  </li>
+                  <li>
+                    <img src={cast2} />
+                  </li>
+                  <li>
+                    <img src={cast3} />
+                  </li>
+                  <li>
+                    <img src={cast4} />
+                  </li>
+                  <li>
+                    <img src={cast5} />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Wrap>
     </>
   );
 };
--- Movie_Card_UI(23_08_22)/src/styles.jsx
+++ Movie_Card_UI(23_08_22)/src/styles.jsx
@@ -1,5 +1,153 @@
-import styled,{createGlobalStyle} from "styled-components"
+import styled, { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle``
+export const GlobalStyle = createGlobalStyle`
+@import url("https://fonts.googleapis.com/css?family=PT+Sans");
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "PT Sans", sans-serif;
+  }
 
-export const Wrap = styled.div``
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background: linear-gradient(#007bb2, #0d1423);
+  }
+`;
+
+export const Wrap = styled.div`
+  .card {
+    position: relative;
+    width: 320px;
+    height: 450px;
+    border-radius: 20px;
+    overflow: hidden;
+    box-shadow: 0 15px 35px rgba(0, 0, 0, 0.25);
+
+    &:hover {
+      .poster {
+        &::before {
+          bottom: 0;
+        }
+
+        img {
+          transform: translateY(-50px);
+          filter: blur(5px);
+        }
+      }
+
+      .details {
+        bottom: 0;
+      }
+    }
+
+    .poster {
+      position: relative;
+      overflow: hidden;
+
+      &::before {
+        content: "";
+        position: absolute;
+        bottom: -180px;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(0deg, #1064a6 50%, transparent);
+        transition: 0.5s;
+        z-index: 1;
+      }
+
+      img {
+        transition: 0.5s;
+        width: 100%;
+      }
+    }
+
+    .details {
+      position: absolute;
+      width: 100%;
+      bottom: -160px;
+      left: 0;
+      padding: 20px;
+      z-index: 2;
+      transition: 0.5s;
+
+      .logo {
+        max-width: 180px;
+      }
+
+      h3 {
+        font-size: 0.8em;
+        color: #fff;
+      }
+
+      .rating {
+        position: relative;
+        padding: 5px 0;
+
+        .fa-star {
+          color: #f7f406;
+          font-size: 1em;
+        }
+
+        span {
+          color: #fff;
+          margin-left: 5px;
+        }
+      }
+
+      .tags {
+        position: relative;
+        margin-top: 5px;
+
+        span {
+          padding: 2px 5px;
+          margin-right: 5px;
+          color: #fff;
+          background: #03a8f5;
+          border-radius: 4px;
+
+          &:nth-child(2) {
+            background: #ff5722;
+          }
+        }
+      }
+
+      .info {
+        color: #fff;
+        margin-top: 20px;
+      }
+
+      .cast {
+        position: relative;
+
+        h4 {
+          color: #fff;
+          margin-top: 10px;
+        }
+
+        ul {
+          position: relative;
+          display: flex;
+          gap: 10px;
+          margin-top: 10px;
+
+          li {
+            list-style: none;
+            width: 35px;
+            height: 35px;
+            overflow: hidden;
+            border-radius: 50%;
+            border: 2px solid white;
+
+            img {
+              max-width: 100%;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
