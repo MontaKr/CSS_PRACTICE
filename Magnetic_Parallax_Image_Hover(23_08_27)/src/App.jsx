176e985042e3df766cf3882d31a0220126d7d7d9
임지훈 <zhoon95@gmail.com>
2023-08-28 16:17:19 +0900

    REACT

--- Magnetic_Parallax_Image_Hover(23_08_27)/src/App.jsx
+++ Magnetic_Parallax_Image_Hover(23_08_27)/src/App.jsx
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useRef } from "react";
 import { GlobalStyle, Wrap } from "./styles";
 
 const App = () => {
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+
+  const mouseMoveFunc = (e, ref) => {
+    let x = e.nativeEvent.offsetX;
+    let y = e.nativeEvent.offsetY;
+    let cardWidth = ref.current.clientWidth;
+    let cardHeight = ref.current.clientHeight;
+    let transX = x - cardWidth / 2;
+    let transY = y - cardHeight / 2;
+
+    ref.current.style.transform = `translate(${transX}px, ${transY}px)`;
+    ref.current.style.backgroundPosition = `${-x / 2}px ${-y / 2}px`;
+  };
+
+  const mouseOutFunc = (ref) => {
+    ref.current.style.transform = "";
+    ref.current.style.backgroundPosition = "";
+  };
+
   return (
     <>
       <GlobalStyle />
-      <Wrap></Wrap>
+      <Wrap>
+        <div className="container">
+          <div
+            onMouseMove={(e) => {
+              mouseMoveFunc(e, ref1);
+            }}
+            onMouseOut={() => {
+              mouseOutFunc(ref1);
+            }}
+            ref={ref1}
+            className="card img1"
+          >
+            <h2>Image 01</h2>
+          </div>
+          <div
+            onMouseMove={(e) => {
+              mouseMoveFunc(e, ref2);
+            }}
+            onMouseOut={() => {
+              mouseOutFunc(ref2);
+            }}
+            ref={ref2}
+            className="card img2"
+          >
+            <h2>Image 02</h2>
+          </div>
+        </div>
+      </Wrap>
     </>
   );
 };
--- Magnetic_Parallax_Image_Hover(23_08_27)/src/styles.jsx
+++ Magnetic_Parallax_Image_Hover(23_08_27)/src/styles.jsx
@@ -1,5 +1,75 @@
 import styled, { createGlobalStyle } from "styled-components";
+import img1 from "./img1.jpg";
+import img2 from "./img2.jpg";
 
-export const GlobalStyle = createGlobalStyle``;
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: consolas;
+  }
 
-export const Wrap = styled.div``;
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background: #0f222f;
+  }
+`;
+
+export const Wrap = styled.div`
+  .container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 150px;
+
+    .card {
+      position: relative;
+      width: 300px;
+      height: 300px;
+      background: var(--img);
+      background-size: 650px;
+      background-position: center;
+      background-repeat: no-repeat;
+      cursor: pointer;
+      transition: 0.2s;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      &.img1 {
+        background: url(${img1});
+        background-size: 650px;
+        background-position: center;
+      }
+
+      &.img2 {
+        background: url(${img2});
+        background-size: 650px;
+        background-position: center;
+      }
+
+      h2 {
+        background: #fff;
+        color: #0f222f;
+        padding: 5px 25px;
+        font-size: 2em;
+        text-transform: uppercase;
+        letter-spacing: 0.1em;
+        box-shadow: 0 5px 25px rgba(0, 0, 0, 0.25);
+        pointer-events: none;
+        opacity: 0;
+        transition: 0.5s;
+      }
+
+      &:hover h2 {
+        opacity: 1;
+      }
+    }
+  }
+`;
