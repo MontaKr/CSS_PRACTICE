8532d9c20cb1e6b09b2e3bba1a86b2641ec2ef40
임지훈 <zhoon95@gmail.com>
2023-09-03 15:21:59 +0900

    REVISION

--- Background_Parallax_Effect(23_08_29)/src/App.jsx
+++ Background_Parallax_Effect(23_08_29)/src/App.jsx
@@ -1,11 +1,65 @@
 import React from "react";
-import { GlobalStyle, Wrap } from "./styles";
+import { GlobalStyle, Wrap, Img } from "./styles";
+import img1 from "./1.png";
+import img2 from "./2.png";
+import img3 from "./3.png";
+import img4 from "./4.png";
+import img5 from "./5.png";
+import img6 from "./6.png";
+import img7 from "./7.png";
+import img8 from "./8.png";
+import img9 from "./9.png";
+import img10 from "./10.png";
+import img11 from "./11.png";
+import img12 from "./12.png";
 
 const App = () => {
+  const imgArray = [
+    { src: img1, speed: "-5" },
+    { src: img2, speed: "5" },
+    { src: img3, speed: "2" },
+    { src: img4, speed: "6" },
+    { src: img5, speed: "8" },
+    { src: img6, speed: "-2" },
+    { src: img7, speed: "4" },
+    { src: img8, speed: "-9" },
+    { src: img9, speed: "6" },
+    { src: img10, speed: "-7" },
+    { src: img11, speed: "-5" },
+    { src: img12, speed: "5" },
+  ];
+
+  const parallax = (e) => {
+    e.currentTarget.querySelectorAll(".layer").forEach((layer) => {
+      const speed = layer.getAttribute("speed");
+
+      const x = (window.innerWidth - e.pageX * speed) / 100;
+      const y = (window.innerHeight - e.pageY * speed) / 100;
+
+      layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+  };
+
   return (
     <>
       <GlobalStyle />
-      <Wrap></Wrap>
+      <Wrap>
+        <section onMouseMove={parallax}>
+          {imgArray.map((val, idx) => {
+            return (
+              <Img
+                className="layer"
+                speed={val.speed}
+                src={val.src}
+                key={idx}
+              />
+            );
+          })}
+          <h2 className="layer" speed="-2">
+            Parallax
+          </h2>
+        </section>
+      </Wrap>
     </>
   );
 };
--- Background_Parallax_Effect(23_08_29)/src/styles.jsx
+++ Background_Parallax_Effect(23_08_29)/src/styles.jsx
@@ -1,5 +1,44 @@
 import styled, { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle``;
+export const GlobalStyle = createGlobalStyle`
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap");
 
-export const Wrap = styled.div``;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+  }
+
+  body {
+    background-color: #111;
+  }
+
+`;
+
+export const Wrap = styled.div`
+  section {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h2 {
+      position: relative;
+      color: #fff;
+      font-size: 12em;
+    }
+  }
+`;
+
+export const Img = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
