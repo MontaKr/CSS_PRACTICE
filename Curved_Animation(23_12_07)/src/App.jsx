1e655dfab94a9ab3aefc41f5bc407bc76ddc9517
임지훈 <zhoon95@gmail.com>
2023-12-08 18:08:52 +0900

    REACT

--- Curved_Animation(23_12_07)/src/App.jsx
+++ Curved_Animation(23_12_07)/src/App.jsx
@@ -1,11 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import { GlobalStyle, Wrap } from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faPenToSquare,
+  faClone,
+  faLink,
+  faRss,
+} from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
+  const array = [
+    { label: "tap1", icon: faHouse, text: "Home" },
+    { label: "tap2", icon: faPenToSquare, text: "Edit" },
+    { label: "tap3", icon: faClone, text: "Components" },
+    { label: "tap4", icon: faLink, text: "Links" },
+    { label: "tap5", icon: faRss, text: "Feed" },
+  ];
+
+  const inputArray = [
+    { id: "tap1" },
+    { id: "tap2" },
+    { id: "tap3" },
+    { id: "tap4" },
+    { id: "tap5" },
+  ];
+
+  const [selectedTab, setSelectedTab] = useState("tap1");
+
+  const handleChange = (event) => {
+    setSelectedTab(event.target.id);
+  };
+
+  const getSliderStyle = () => {
+    switch (selectedTab) {
+      case "tap1":
+        return { width: "135px", transform: "translateX(0)" };
+      case "tap2":
+        return { width: "110px", transform: "translateX(150px)" };
+      case "tap3":
+        return { width: "210px", transform: "translateX(275px)" };
+      case "tap4":
+        return { width: "120px", transform: "translateX(505px)" };
+      case "tap5":
+        return { width: "130px", transform: "translateX(640px)" };
+      default:
+        return {};
+    }
+  };
+
   return (
     <>
       <GlobalStyle />
-      <Wrap></Wrap>
+      <Wrap>
+        {inputArray.map((val, idx) => {
+          return (
+            <input
+              key={val.id}
+              type="radio"
+              id={val.id}
+              name="tap"
+              checked={selectedTab === val.id}
+              onChange={handleChange}
+            />
+          );
+        })}
+        <nav>
+          <div className="logo">LOGO</div>
+          <ol>
+            {array.map((val, idx) => {
+              return (
+                <li key={val.label}>
+                  <label htmlFor={val.label}>
+                    <a
+                      style={{
+                        color:
+                          selectedTab === val.label ? "#66ff00" : "#002222",
+                      }}
+                    >
+                      <FontAwesomeIcon icon={val.icon} />
+                      {val.text}
+                    </a>
+                  </label>
+                </li>
+              );
+            })}
+            <div className="slider" style={getSliderStyle()}></div>
+          </ol>
+        </nav>
+      </Wrap>
     </>
   );
 };
--- Curved_Animation(23_12_07)/src/styles.jsx
+++ Curved_Animation(23_12_07)/src/styles.jsx
@@ -1,5 +1,97 @@
 import styled, { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle``;
+export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Jua&family=Poppins:wght@300;500;600;800;900&display=swap');
 
-export const Wrap = styled.div``;
+
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #002222;
+  }
+  
+`;
+
+export const Wrap = styled.div`
+  font-family: "Poppins", sans-serif;
+
+  nav {
+    background-color: #66ff00;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 80px;
+
+    .logo {
+      font-size: 2.3em;
+      font-weight: 800;
+      color: #002222;
+      letter-spacing: 2px;
+    }
+
+    ol {
+      display: flex;
+      position: relative;
+      margin: auto 0;
+
+      li {
+        margin: 0 30px;
+        list-style: none;
+        z-index: 111;
+
+        a {
+          display: block;
+          color: #002222;
+          font-size: 1.5em;
+          text-decoration: none;
+          transition: 0.2s 0.1s ease;
+        }
+      }
+
+      .slider {
+        position: absolute;
+        height: 75px;
+        width: 150px;
+        left: 10px;
+        top: -24px;
+        background-color: #002222;
+        border-radius: 15px 15px 0 0;
+        transform: translateX(0);
+        transition: 0.2s 0.1s ease;
+
+        &::before {
+          content: "";
+          position: absolute;
+          height: 25px;
+          width: 25px;
+          background-color: #66ff00;
+          bottom: 0;
+          left: -25px;
+          border-bottom-right-radius: 15px;
+          box-shadow: 13px 0 0 #002222;
+        }
+
+        &::after {
+          content: "";
+          position: absolute;
+          height: 25px;
+          width: 25px;
+          background-color: #66ff00;
+          bottom: 0;
+          right: -25px;
+          border-bottom-left-radius: 15px;
+          box-shadow: -13px 0 0 #002222;
+        }
+      }
+    }
+  }
+
+  input {
+    display: none;
+  }
+`;
