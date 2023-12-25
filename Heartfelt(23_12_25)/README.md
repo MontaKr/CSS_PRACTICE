a62a19236b86e713bbdead200bd1b9cab5e32dfa
MontaKr <zhoon95@gmail.com>
2023-12-25 20:13:17 +0900

    HEARTFELT

---
+++ Heartfelt(23_12_25)/README.md
@@ -0,0 +1,6 @@
+# Heartfelt
+
+
+### CodeSandBox
+JavaScript : [https://codesandbox.io/s/heartfelt-8d4jmf](https://codesandbox.io/s/heartfelt-8d4jmf) \
+React : []()
---
+++ Heartfelt(23_12_25)/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { GlobalStyle, Wrap } from './styles'
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle/>
+      <Wrap>
+      </Wrap>
+    </>
+  )
+}
+
+export default App
