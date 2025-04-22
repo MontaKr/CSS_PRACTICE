3ca1ea0ea0e03776263707bae4a2373be0e1fba4
ZHOON <zhoon95@gmail.com>
2025-04-22 09:38:12 +0900

    HOVER EFFECT

---
+++ image_hover_effect/src/app/page.js
@@ -0,0 +1,9 @@
+import InversionLens from "@/components/InversionLens/InversionLens";
+
+export default function Home() {
+  return (
+    <div>
+      <InversionLens src="/portrait.jpeg" />
+    </div>
+  );
+}
